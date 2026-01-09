50eb68bff1df8131f2176c5524715442ec368a16
Jaclo-00 <jordanex4u@gmail.com>
2026-01-09 15:53:30 -0500

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ChrisTitusTech/bypassnro/refs/heads/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/Jaclo-00/bypassnro/refs/heads/main/main-basic/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
